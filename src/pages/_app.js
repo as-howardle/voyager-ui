4a7abc717fc8a1d909ba69c8727c6484b46a977b
howardle1806 <115439524+howardle1806@users.noreply.github.com>
2023-04-27 16:23:05 +0700

    change title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -31,7 +31,7 @@ const App = (props) => {
     <CacheProvider value={emotionCache}>
       <Head>
         <title>
-          Devias Kit
+          Janus
         </title>
         <meta
           name="viewport"
